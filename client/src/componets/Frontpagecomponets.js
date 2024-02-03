455bf187380849ed414238c2390ef3b95f6f2d77
aniket2727 <aniketkadam5950@gmail.com>
2024-02-03 19:13:29 +0530

    changes related to frontpage ui

--- client/src/componets/Frontpagecomponets.js
+++ client/src/componets/Frontpagecomponets.js
@@ -4,9 +4,7 @@ import { logoutUser } from '../features/userSlice';
 
 const Frontpagecomponets = () => {
   const userEmail = useSelector((state) => state.user.email);
-  const userToken = useSelector((state) => state.user.token); // Add this line
-  const dispatch = useDispatch();
-
+ 
   const handleLogout = () => {
     // Dispatch the logout action to clear user data in the state
     dispatch(logoutUser());
