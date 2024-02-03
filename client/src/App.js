c718be8ea9d9db68743c8874780949ac118fa6ee
aniket2727 <aniketkadam5950@gmail.com>
2024-02-03 19:03:27 +0530

    changs in app componets related to page not found

--- client/src/App.js
+++ client/src/App.js
@@ -1,22 +1,21 @@
-
-
-
+// App.js
+import React from 'react';
 import Frontpagecomponets from "./componets/Frontpagecomponets";
 import LoginComponent from "./componets/Logincomponets";
 import Navbarcomponets from "./componets/Header/Navbarcomponets";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 function App() {
   return (
     <div>
-
       <BrowserRouter>
         <Navbarcomponets />
         <Routes>
           <Route path="/primesport/login" element={<LoginComponent />} />
           <Route path="/primesport/productpagefirst" element={<Frontpagecomponets />} />
+          <Route path="*" element={<div><h2>Page Not Found</h2><p>The requested page does not exist.</p></div>} />
         </Routes>
       </BrowserRouter>
-
     </div>
   );
 }
