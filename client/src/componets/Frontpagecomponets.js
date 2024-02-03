afe3a52d87cc642506c3e9235a7558889289174f
aniket2727 <aniketkadam5950@gmail.com>
2024-02-03 19:16:46 +0530

    changes related to manage logout in navbar and frontpage

--- client/src/componets/Frontpagecomponets.js
+++ client/src/componets/Frontpagecomponets.js
@@ -1,22 +1,12 @@
 import React from 'react';
-import { useSelector, useDispatch } from 'react-redux';
-import { logoutUser } from '../features/userSlice';
 
 const Frontpagecomponets = () => {
-  const userEmail = useSelector((state) => state.user.email);
  
-  const handleLogout = () => {
-    // Dispatch the logout action to clear user data in the state
-    dispatch(logoutUser());
-    console.log('logout is called');
-  };
-
+ 
+ 
   return (
     <div>
-      <p>Another Component User Email: {userEmail}</p>
-      <p>User Token: {userToken}</p>
-
-      <button onClick={handleLogout}>Logout</button>
+       <h1>aniket</h1>
     </div>
   );
 };
--- client/src/componets/Header/Navbarcomponets.js
+++ client/src/componets/Header/Navbarcomponets.js
@@ -1,11 +1,19 @@
 import React from 'react';
-import { useSelector } from 'react-redux';
+import { useSelector,useDispatch } from 'react-redux';
+import { logoutUser } from '../../features/userSlice';
 import { FaUser, FaShoppingCart, FaShoppingBag } from 'react-icons/fa';
 import './navbar.css';
 
 const Navbarcomponets = () => {
+  const dispatch=useDispatch()
   var userEmail = useSelector((state) => state.user.email);
   userEmail="aniket"
+  
+  const handleLogout = () => {
+    dispatch(logoutUser());
+    console.log('logout is called');
+  };
+
 
   return (
     <div className='main-navbar-a'>
@@ -21,7 +29,7 @@ const Navbarcomponets = () => {
         <div className='navbar-login-status-a'>
           {
             userEmail ? (
-              <button className='logout-btn'>
+              <button className='logout-btn' onClick={handleLogout}>
                 <FaUser />
                 Logout
               </button>
